c0ec511475e160fddf8a398584c748fb1f92b6b4
Annelle <gghaslam@gmail.com>
2023-03-11 11:56:33 -0500

    github footer

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import githubImage from '../../assets/github.png'
+export default function App() {
+  return (
+    
+
+      <div>
+        © 2023 Copyright:
+        <a href="https://github.com/GayeGH">
+          <img src={githubImage} alt="github"/>
+          </a>
+          
+    
+      </div>
+    
+  );
+}
