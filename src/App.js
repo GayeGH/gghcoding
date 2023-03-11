faf2a29b6f713208c04730bf3cf1e6d41a7d9c52
Annelle <gghaslam@gmail.com>
2023-03-11 11:33:59 -0500

    adding images, repos, correcting indes.css

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
 const App = () => <PortfolioContainer/>;
-// import './App.css';
-// import Header from './components/pages/Home';
-// import Footer from './components/pages/Contact';
-// import Navigation from './components/pages/Navigation';
-// import Portfolio from './components/pages/Portfolio';
-// import About from './components/pages/About'
-// function App() {
-//   return (
-//     <div className="App">
-//        <Header/> 
+//  import './App.css';
+//  import Header from './components/pages/Home';
+//  import Footer from './components/pages/Contact';
+//  import Navigation from './components/pages/Navigation';
+//  import Portfolio from './components/pages/Portfolio';
+//  import About from './components/pages/About'
+//  function App() {
+//    return (
+//      <div className="App">
+//         <Header/> 
 //        <About/>
-//        <Portfolio/>
+//         <Portfolio/>
        
 //        <Footer/> 
-//     </div>
+//      </div>
 //   );
-// }
+//  }
+
+
 
 export default App;
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,41 +1,53 @@
 import React from 'react';
-
+import pwgenImage from '../../assets/pwgen.png';
+import petprojectImage from '../../assets/petproject.png';
+import mealstottiesImage from '../../assets/mealstotties.png';
+import weatherappImage from '../../assets/weatherapp.png';
+import employeetrackerImage from '../../assets/employeetracker.png';
+import notetakerImage from '../../assets/notetaker.png';
+import teamprofgenImage from '../../assets/teamprofgen.png';
 function Portfolio() {
   return (
-    <div>
-      <div id="portfolio" className="card">
-        <p>
+    
+      <div id="portfolio">
+        
+        
           <h2>Github Repositories</h2>
           <h2>Feel free to review projects on Github, such as...</h2>
-          <a href="https://github.com/GayeGH/Horiseon">
-            <img
-              src="https://i.imgur.com/2xMnUTm.png"
-              width="400"
-              height="300"
-              alt="Horiseon logo"
-            />
+          
+          <a href="https://github.com/GayeGH/Employee-Tracker">
+            <img src={employeetrackerImage} alt="employeetracker" id="largeportfoliopic"/>
+          </a>
+          
+        <div id="smallportpics">
+          <a href="https://github.com/Lwalsh2022/Pet-Project">
+          <img src={petprojectImage} alt="petproject" className="smallportfoliopic"/>
+          
+          </a>
+          <a  href="https://github.com/GayeGH/Password-Generator">
+          <img src={pwgenImage} alt="pwgen" className="smallportfoliopic"/>
           </a>
 
           <a href="https://github.com/GhostoftheMill/project-p1g5">
-            <img
-              src="https://i.imgur.com/JjHf0vJ.png"
-              width="400"
-              height="300"
-              alt="Meals and Totties"
-            />
+          <img src={mealstottiesImage} alt="mealstotties" className="smallportfoliopic"/>
           </a>
 
-          <a href="https://github.com/Lwalsh2022/Pet-Project">
-            <img
-              src="https://imgur.com/vOB8BG9.png"
-              width="400"
-              height="300"
-              alt="Pet-Project"
-            />
+         <a  href="https://github.com/GayeGH/Weather-App">
+          <img src={weatherappImage} alt="weatherapp" className="smallportfoliopic"/>
           </a>
-        </p>
+
+          <a href="https://github.com/GayeGH/Note-Taker1">
+            <img src={notetakerImage} alt="notetaker" className="smallportfoliopic"/>
+            </a>
+
+         <a href="https://github.com/GayeGH/Team-Profile-Generator">
+            <img src={teamprofgenImage}   alt="teamprofgen" className="smallportfoliopic"/>
+         </a>
+        </div>
+        
       </div>
-    </div>
+    
   );
 }
+
 export default Portfolio;
